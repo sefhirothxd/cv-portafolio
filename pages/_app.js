67bf8f14eababc8524a40892489ab695b360e6cc
bryan vera <bveracachay@gmail.com>
2021-07-19 19:45:16 -0500

    agregando cambios en el fondo

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,53 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
 import generateStore from '../redux/store';
-import Fondo from '../Layout/Fondo';
+import Particles from 'react-particles-js';
+// import Fondo from '../Layout/Fondo';
 
 const store = generateStore();
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Provider store={store}>
-			<Fondo>
-				<Component {...pageProps} />
-			</Fondo>
+			<Particles
+				className="h-screen w-full fixed z-0 "
+				id="particles-js"
+				params={{
+					particles: {
+						number: {
+							value: 40,
+							density: {
+								enable: true,
+								value_area: 1000,
+							},
+						},
+						color: {
+							value: '#fff',
+						},
+						opacity: {
+							value: 0.5,
+							anim: {
+								enable: true,
+							},
+						},
+						size: {
+							value: 7,
+							random: true,
+							anim: {
+								enable: true,
+								speed: 3,
+							},
+						},
+						line_linked: {
+							enable: false,
+						},
+						move: {
+							speed: 0.2,
+						},
+					},
+				}}
+			/>
+			<Component {...pageProps} />
 			<ToastContainer
 				position="top-right"
 				autoClose={5000}
