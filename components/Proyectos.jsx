bee77fab6cd9a380bff2c60f7d4a086482bfce1a
Bryan Vera <bveracachay@gmail.com>
2021-10-23 11:15:50 -0500

    se agrega ultimo proyecto

--- components/Proyectos.jsx
+++ components/Proyectos.jsx
@@ -51,9 +51,8 @@ function Proyectos() {
 			<div className="bg-white bg-opacity-20 border-t-4 border-l-4  rounded-3xl p-6 md:p-10 lg:p-20 lg:mt-20 mt-10 mb-20">
 				<img className="mx-auto mb-6" src="/photo1.png" alt="" />
 				<p className=" lg:max-w-960 lg:mx-auto sm:text-center md:text-2xl  lg:text-34px lg:leading-normal mb-5 text-textColor text-base leading-6 font-body font-bold">
-					"Bryan ha sido de gran ayuda para acelerar ciertos esfuerzos de
-					desarrollo. Lo que nos hubiera llevado 6 meses solo les llevó 1,5
-					meses ".
+					"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum Lorem ipsum lorem
+					ipsum lorem ipsum lorem ipsum".
 				</p>
 				<p className="text-textColor opacity-50 text-xs leading-6 font-body font-normal lg:text-2xl sm:text-center lg:font-bold">
 					LEONIDAS ESTEBAN / COFUNDADOR Y CEO LEONIDASESTEBAN.COM
@@ -97,33 +96,44 @@ function Proyectos() {
 			<div className="bg-white bg-opacity-20 border-t-4 border-l-4  rounded-3xl p-6 md:p-10 lg:p-20 lg:mt-20 mt-10 mb-20">
 				<img className="mx-auto mb-6" src="/user-2.svg" alt="" />
 				<p className=" lg:max-w-960 lg:mx-auto sm:text-center md:text-2xl  lg:text-34px lg:leading-normal mb-5 text-textColor text-base leading-6 font-body font-bold">
-					"Bryan ha superado mis expectativas desde el diseño hasta el
-					desarrollo".
+					"Lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit
+					amet, consectetur adip".
 				</p>
 				<p className="text-textColor opacity-50 text-xs leading-6 font-body font-normal lg:text-2xl sm:text-center lg:font-bold">
 					JASON LOPEZ / COFUNDADOR Y CEO MARVOL
 				</p>
 			</div>
 			<div className="lg:flex-row-reverse lg:justify-between lg:pb-20 pb-10 flex justify-center flex-col items-center ">
-				<img src="/iphone.svg" alt="" className="mb-8" />
+				<img
+					src="/kitoperu.png"
+					alt=""
+					className="mb-8 w-1/2 lg:2/6 rounded-xl "
+				/>
 				<div className="lg:pb-20">
 					<article className="mb-5 max-w-md">
 						<h3 className="shadow-showTitle py-4 lg:text-34px mb-5 inline-block bg-greenLight  px-2 text-black text-2xl">
-							NETNET
+							KITO PERU
 						</h3>
 						<p className="text-textColor text-base leading-6 font-body font-normal lg:pr-4">
-							Películas y series ilimitadas y mucho más. Disfruta donde quieras.
-							Cancela cuando quieras.
+							Empresa dedicada a la venta de herramientas industriales.
 						</p>
 					</article>
-					<div className="lg:flex lg:flex-row-reverse lg:max-w-340">
-						<button className="flex mb-4 text-textColor lg:mb-0 lg:w-30 items-center justify-center font-body text-14px font-bold border-textColor border-2 rounded-full w-full py-3">
+					<div className="flex lg:flex-row-reverse lg:max-w-340 flex-col text-center ">
+						<a
+							target="_blank"
+							href="https://github.com/sefhirothxd/kitoperu"
+							className="flex mb-4 text-textColor lg:mb-0 lg:w-30 items-center justify-center font-body text-14px font-bold border-textColor border-2 rounded-full w-full py-3"
+						>
 							<img className="inline mr-2" src="/icon-github.svg" />
 							VER CÓDIGO
-						</button>
-						<button className="bg-greenLight lg:mr-3 lg:w-30 font-body text-14px font-bold border-textColor border-2 rounded-full w-full py-3">
+						</a>
+						<a
+							target="_blank"
+							href="https://kitoperu.vercel.app/"
+							className="bg-greenLight lg:mr-3 lg:w-30 font-body text-14px font-bold border-textColor border-2 rounded-full w-full py-3"
+						>
 							VER PROYECTO
-						</button>
+						</a>
 					</div>
 				</div>
 			</div>
@@ -134,7 +144,7 @@ function Proyectos() {
 					todas las plataformas en las que trabajamos".
 				</p>
 				<p className="text-textColor opacity-50 text-xs leading-6 font-body font-normal lg:text-2xl sm:text-center lg:font-bold">
-					MARCO CORTEZ / NETNET
+					ANTONY / KITO PERU
 				</p>
 			</div>
 		</div>
